24cbe5cf14c44e6c61425092dcb523ab94cc9c7e
zardron <zardron.pesquera1993@gmail.com>
2023-05-10 14:52:55 +0400

    finalizing the frontend code

---
+++ client/src/features/auth/Welcome.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser, selectCurrentToken } from "./authSlice";
+import { Link } from "react-router-dom";
+
+const Welcome = () => {
+  const user = useSelector(selectCurrentUser);
+  const token = useSelector(selectCurrentToken);
+
+  const welcome = user ? `Welcome ${user}` : "Welcome";
+  const tokenAbbr = `${token.slice(0, 9)}...`;
+
+  const contet = (
+    <section className="welcome">
+      <h1>{welcome}</h1>
+      <p>Token: {tokenAbbr}</p>
+      <p>
+        <Link to="/userslist">Go to the Users List</Link>
+      </p>
+    </section>
+  );
+  return contet;
+};
+
+export default Welcome;
